a36e635a6f4fff2caeac5d913e86a7d3e344c58a
limi-projects <142051883+limi-projects@users.noreply.github.com>
2025-05-18 20:51:13 +0100

    Update pendulum.py

--- docs/physics/pendulum.py
+++ docs/physics/pendulum.py
@@ -1,1 +1,6 @@
-TO DO
+# TO DO: Create a database of equations. Incorportate this into a general function. 
+from equations import evaluate_equation# See limi-utils
+def pendulum(get: str, given: dict, stdout=True, pretty=True) -> float:
+    equation = 'T - 2 * pi * (L/g)**0.5'
+    result = evaluate_equation(equation, get, given, stdout, pretty)
+    return result
